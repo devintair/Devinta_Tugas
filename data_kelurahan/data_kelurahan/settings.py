d34e95d95c75ed2479132b565cd9e3f3e5e67a7e
devintair <santidevintairma@gmail.com>
2025-11-18 19:49:22 +0700

    Pertemuan 12

--- data_kelurahan/data_kelurahan/settings.py
+++ data_kelurahan/data_kelurahan/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'django_filters',
     'corsheaders',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -137,6 +138,13 @@ REST_FRAMEWORK = {
      # Tambahkan konfigurasi di bawah ini
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10, # Jumlah item per halaman
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema', # Tambahkan ini
 
 }
 CORS_ALLOW_ALL_ORIGINS = True
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'API Aplikasi Warga Kelurahan',
+    'DESCRIPTION': 'Dokumentasi API untuk mengelola data warga dan pengaduan.',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
